3a581dbf30eb58df7c056977554d6920ba6a2b92
f-fl0 <66578286+f-fl0@users.noreply.github.com>
2020-11-29 13:59:15 +0900

    Fix calls to Python2 print function

--- superpoint/utils/loss_functions/loss_composer.py
+++ superpoint/utils/loss_functions/loss_composer.py
@@ -4,7 +4,7 @@ from dense_correspondence.loss_functions.pixelwise_contrastive_loss import Pixel
 import torch
 from torch.autograd import Variable
 
-def get_loss(pixelwise_contrastive_loss, match_type, 
+def get_loss(pixelwise_contrastive_loss, match_type,
               image_a_pred, image_b_pred,
               matches_a,     matches_b,
               masked_non_matches_a, masked_non_matches_b,
@@ -22,7 +22,7 @@ def get_loss(pixelwise_contrastive_loss, match_type,
 
     """
     if (match_type == SpartanDatasetDataType.SINGLE_OBJECT_WITHIN_SCENE).all():
-        print "applying SINGLE_OBJECT_WITHIN_SCENE loss"
+        print("applying SINGLE_OBJECT_WITHIN_SCENE loss")
         return get_within_scene_loss(pixelwise_contrastive_loss, image_a_pred, image_b_pred,
                                             matches_a,    matches_b,
                                             masked_non_matches_a, masked_non_matches_b,
@@ -30,18 +30,18 @@ def get_loss(pixelwise_contrastive_loss, match_type,
                                             blind_non_matches_a, blind_non_matches_b)
 
     if (match_type == SpartanDatasetDataType.SINGLE_OBJECT_ACROSS_SCENE).all():
-        print "applying SINGLE_OBJECT_ACROSS_SCENE loss"
+        print("applying SINGLE_OBJECT_ACROSS_SCENE loss")
         return get_same_object_across_scene_loss(pixelwise_contrastive_loss, image_a_pred, image_b_pred,
                                             blind_non_matches_a, blind_non_matches_b)
 
     if (match_type == SpartanDatasetDataType.DIFFERENT_OBJECT).all():
-        print "applying DIFFERENT_OBJECT loss"
+        print("applying DIFFERENT_OBJECT loss")
         return get_different_object_loss(pixelwise_contrastive_loss, image_a_pred, image_b_pred,
                                             blind_non_matches_a, blind_non_matches_b)
 
 
     if (match_type == SpartanDatasetDataType.MULTI_OBJECT).all():
-        print "applying MULTI_OBJECT loss"
+        print("applying MULTI_OBJECT loss")
         return get_within_scene_loss(pixelwise_contrastive_loss, image_a_pred, image_b_pred,
                                             matches_a,    matches_b,
                                             masked_non_matches_a, masked_non_matches_b,
@@ -49,7 +49,7 @@ def get_loss(pixelwise_contrastive_loss, match_type,
                                             blind_non_matches_a, blind_non_matches_b)
 
     if (match_type == SpartanDatasetDataType.SYNTHETIC_MULTI_OBJECT).all():
-        print "applying SYNTHETIC_MULTI_OBJECT loss"
+        print("applying SYNTHETIC_MULTI_OBJECT loss")
         return get_within_scene_loss(pixelwise_contrastive_loss, image_a_pred, image_b_pred,
                                             matches_a,    matches_b,
                                             masked_non_matches_a, masked_non_matches_b,
@@ -78,16 +78,16 @@ def get_within_scene_loss(pixelwise_contrastive_loss, image_a_pred, image_b_pred
 
     if pcl._config["use_l2_pixel_loss_on_background_non_matches"]:
         background_non_match_loss, num_background_hard_negatives =\
-            pixelwise_contrastive_loss.non_match_loss_with_l2_pixel_norm(image_a_pred, image_b_pred, matches_b, 
-                background_non_matches_a, background_non_matches_b, M_descriptor=pcl._config["M_background"])    
-        
+            pixelwise_contrastive_loss.non_match_loss_with_l2_pixel_norm(image_a_pred, image_b_pred, matches_b,
+                background_non_matches_a, background_non_matches_b, M_descriptor=pcl._config["M_background"])
+
     else:
         background_non_match_loss, num_background_hard_negatives =\
             pixelwise_contrastive_loss.non_match_loss_descriptor_only(image_a_pred, image_b_pred,
                                                                     background_non_matches_a, background_non_matches_b,
                                                                     M_descriptor=pcl._config["M_background"])
-        
-        
+
+
 
     blind_non_match_loss = zero_loss()
     num_blind_hard_negatives = 1
@@ -96,7 +96,7 @@ def get_within_scene_loss(pixelwise_contrastive_loss, image_a_pred, image_b_pred
             pixelwise_contrastive_loss.non_match_loss_descriptor_only(image_a_pred, image_b_pred,
                                                                     blind_non_matches_a, blind_non_matches_b,
                                                                     M_descriptor=pcl._config["M_masked"])
-        
+
 
 
     total_num_hard_negatives = num_masked_hard_negatives + num_background_hard_negatives
@@ -131,7 +131,7 @@ def get_within_scene_loss(pixelwise_contrastive_loss, image_a_pred, image_b_pred
     loss = pcl._config["match_loss_weight"] * match_loss + \
     pcl._config["non_match_loss_weight"] * non_match_loss
 
-    
+
 
     return loss, match_loss, masked_non_match_loss_scaled, background_non_match_loss_scaled, blind_non_match_loss_scaled
 
@@ -143,15 +143,15 @@ def get_within_scene_loss_triplet(pixelwise_contrastive_loss, image_a_pred, imag
     """
     Simple wrapper for pixelwise_contrastive_loss functions.  Args and return args documented above in get_loss()
     """
-    
+
     pcl = pixelwise_contrastive_loss
 
     masked_triplet_loss =\
-        pixelwise_contrastive_loss.get_triplet_loss(image_a_pred, image_b_pred, matches_a, 
+        pixelwise_contrastive_loss.get_triplet_loss(image_a_pred, image_b_pred, matches_a,
             matches_b, masked_non_matches_a, masked_non_matches_b, pcl._config["alpha_triplet"])
-        
+
     background_triplet_loss =\
-        pixelwise_contrastive_loss.get_triplet_loss(image_a_pred, image_b_pred, matches_a, 
+        pixelwise_contrastive_loss.get_triplet_loss(image_a_pred, image_b_pred, matches_a,
             matches_b, background_non_matches_a, background_non_matches_b, pcl._config["alpha_triplet"])
 
     total_loss = masked_triplet_loss + background_triplet_loss
@@ -173,7 +173,7 @@ def get_different_object_loss(pixelwise_contrastive_loss, image_a_pred, image_b_
             pixelwise_contrastive_loss.non_match_loss_descriptor_only(image_a_pred, image_b_pred,
                                                                     blind_non_matches_a, blind_non_matches_b,
                                                                     M_descriptor=M_descriptor)
-        
+
         if scale_by_hard_negatives:
             scale_factor = max(num_hard_negatives, 1)
         else:
